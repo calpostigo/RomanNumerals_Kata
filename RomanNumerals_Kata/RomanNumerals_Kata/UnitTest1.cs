f77b4ba2d656493be8528c65fe9941192c5b27de
calpostigo <ecalojeu@gmail.com>
2022-10-28 17:08:20 +0100

    refactor

--- RomanNumerals_Kata/RomanNumerals_Kata/UnitTest1.cs
+++ RomanNumerals_Kata/RomanNumerals_Kata/UnitTest1.cs
@@ -7,40 +7,15 @@ namespace RomanNumerals_Kata {
         public void Setup() {
         }
 
-        [Test]
-        public void return_roman_numeral_I_for_number_1() {
-            var number = 1;
+        [TestCase(1, "I")]
+        [TestCase(2, "II")]
+        [TestCase(3, "III")]
+        [TestCase(4, "IV")]
+        public void return_expected_roman_numeral_for_a_number(int number, string expected) {
 
             var result = RomanNumerals.RomanNumeralFrom(number);
 
-            result.Should().Be("I");
-        }
-
-        [Test]
-        public void return_roman_numeral_II_for_number_2() {
-            var number = 2;
-
-            var result = RomanNumerals.RomanNumeralFrom(number);
-
-            result.Should().Be("II");
-        }
-
-        [Test]
-        public void return_roman_numeral_III_for_number_3() {
-            var number = 3;
-
-            var result = RomanNumerals.RomanNumeralFrom(number);
-
-            result.Should().Be("III");
-        }
-
-        [Test]
-        public void return_roman_numeral_IV_for_number_4() {
-            var number = 4;
-
-            var result = RomanNumerals.RomanNumeralFrom(number);
-
-            result.Should().Be("IV");
+            result.Should().Be(expected);
         }
     }
 
@@ -48,7 +23,7 @@ namespace RomanNumerals_Kata {
         public static object RomanNumeralFrom(int number) {
             var romanNumeral = string.Empty;
             if (number == 4) return "IV";
-            for (int counter = 1; counter < 4; counter++) {
+            for (int counter = 1; counter <= number; counter++) {
                 romanNumeral += "I";
             }
             return romanNumeral;
