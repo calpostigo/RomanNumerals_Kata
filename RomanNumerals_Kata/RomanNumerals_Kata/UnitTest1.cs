c2a2dc8ff401019e2dcaae72482be5a9b299f149
calpostigo <ecalojeu@gmail.com>
2022-10-28 17:21:44 +0100

    red return_expected_roman_numeral_for_a_number_6

--- RomanNumerals_Kata/RomanNumerals_Kata/UnitTest1.cs
+++ RomanNumerals_Kata/RomanNumerals_Kata/UnitTest1.cs
@@ -12,6 +12,7 @@ namespace RomanNumerals_Kata {
         [TestCase(3, "III")]
         [TestCase(4, "IV")]
         [TestCase(5, "V")]
+        [TestCase(6, "VI")]
         public void return_expected_roman_numeral_for_a_number(int number, string expected) {
 
             var result = RomanNumerals.RomanNumeralFrom(number);
